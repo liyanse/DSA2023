09d576fd4fb93a155b8093fc3ff5f60cb68a61be
liyanse <lianswanjiku@icloud.com>
2023-05-08 12:37:14 +0300

    Create RStudio.R

---
+++ RStudio.R
@@ -0,0 +1,8 @@
+#c()
+## Used to create vectors with commas to separate the numbers
+## seq is used in a sequence of numbers
+## Rep is another common function to create vector which is made of repeated numbers
+
+A=matrix(c(1,2,3,4),nrow=2, ncol=2, byrow=TRUE)
+B=matrix(c(3,4,2,6),nrow=2,byrow=TRUE)
+
